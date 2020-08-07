b20ff7e5359c10cd0f9d75a34ad3a490333d5d90
Gabo2494 <marvingbrl1824@gmail.com>
2020-08-06 22:58:58 -0600

    Segundo laboratorio

---
+++ labs/lab-02/material/WebApi/Controllers/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApi.Infrastructure.Data.Models;
+
+namespace WebApi.Controllers
+{
+    public class ProductRepository
+    {
+        public AdventureworksContext UnitOfWork { get; set; }
+
+        public ProductRepository(AdventureworksContext dbContext)
+        {
+            UnitOfWork = dbContext;
+        }
+
+        private readonly List<string> Products= new List<string>()
+        {
+            "Jeans","T-shirt","Pants"
+        };
+
+        public object[] Get()
+        {
+            int i = 0;
+            return Products.Select(model => new
+            {
+                Name = model,
+                Id = i++
+            }).ToArray();
+        }
+
+        public object Get(int id)
+        {
+            int i = 0;
+            var result = Products.Select(model => new
+            {
+                Name = model,
+                Id = i++
+            }).ToList();
+
+            return result.ElementAtOrDefault(id);
+        }
+
+        public void Save(string name)
+        {
+            //store in DB
+            Products.Add(name);
+
+        }
+
+    }
+
+    
+}
